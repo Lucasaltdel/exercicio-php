6f5e09ed3f64e89e66fcffe7f5756f4452ff639d
Lucasaltdel <89349452+Lucasaltdel@users.noreply.github.com>
2024-07-01 21:20:12 -0300

    Add files via upload

---
+++ Captures - Copia/ex.php
@@ -0,0 +1,10 @@
+<?php
+$cotacao=6;
+
+$valor=1000;
+
+$resultado=1000/6;
+
+echo "o seu valor de R$ $valor em euro que está com a cotação de $cotacao" ."€". " terá o valor de " .number_format($resultado,2 ,",","."). "€"; 
+
+?>
